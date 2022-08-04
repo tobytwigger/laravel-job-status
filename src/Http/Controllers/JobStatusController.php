5364c9573f61dae0a76b87d6b8b4d5bb0b1974bb
Toby Twigger <tobytwigger1@gmail.com>
2022-08-04 02:07:50 +0100

    Update exceptions to http exceptions

--- src/Http/Controllers/JobStatusController.php
+++ src/Http/Controllers/JobStatusController.php
@@ -5,10 +5,10 @@ namespace JobStatus\Http\Controllers;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Validation\UnauthorizedException;
 use JobStatus\Http\Requests\JobStatusSearchRequest;
 use JobStatus\Models\JobStatus;
 use JobStatus\Trackable;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 class JobStatusController extends Controller
 {
@@ -29,13 +29,13 @@ class JobStatusController extends Controller
             ->append(['lastMessage', 'isFinished']);
 
         if(!class_exists($jobStatus->job_class)) {
-            throw new \Exception(sprintf('No job of type %s found', $jobStatus->job_class));
+            return response(sprintf('No job of type %s found', $jobStatus->job_class), 404);
         }
         if(!in_array(Trackable::class, class_uses_recursive($jobStatus->job_class))) {
-            throw new \Exception(sprintf('Job %s is not trackable.', $jobStatus->job_class));
+            return response(sprintf('Job %s is not trackable.', $jobStatus->job_class), 500);
         }
         if(($jobStatus->job_class)::canSeeTracking($this->resolveAuth(), $jobStatus->getTagsAsArray()) === false) {
-            throw new UnauthorizedException('You cannot access this job status');
+            throw new UnauthorizedHttpException('You cannot access this job status');
         }
 
         return $jobStatus;
