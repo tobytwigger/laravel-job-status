2031095eb645bf59866273741db1ba1815eeac48
Toby Twigger <tobytwigger1@gmail.com>
2022-11-19 00:50:12 +0000

    Fix bug

--- src/JobStatusRepository.php
+++ src/JobStatusRepository.php
@@ -9,7 +9,7 @@ class JobStatusRepository
 
     public function getLatestByUuid(string $uuid): ?JobStatus
     {
-        return JobStatus::query()->where('uuid', $job->uuid())->latest()->orderBy('id', 'DESC')->first();
+        return JobStatus::query()->where('uuid', $uuid)->latest()->orderBy('id', 'DESC')->first();
     }
 
 }
