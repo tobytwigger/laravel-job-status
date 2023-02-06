0fc2a0cbb458b079fa972e344eb899aebee30b47
Toby Twigger <tobytwigger1@gmail.com>
2023-02-06 01:02:40 +0000

    Added queue collections

--- tests/Unit/Search/Collections/JobStatusCollectionTest.php
+++ tests/Unit/Search/Collections/JobStatusCollectionTest.php
@@ -7,9 +7,11 @@ use Illuminate\Support\Str;
 use JobStatus\Models\JobBatch;
 use JobStatus\Models\JobStatus;
 use JobStatus\Search\Collections\BatchCollection;
+use JobStatus\Search\Collections\QueueCollection;
 use JobStatus\Search\Collections\JobRunCollection;
 use JobStatus\Search\Collections\TrackedJobCollection;
 use JobStatus\Search\Result\Batch;
+use JobStatus\Search\Result\Queue;
 use JobStatus\Search\Result\JobRun;
 use JobStatus\Search\Result\TrackedJob;
 use JobStatus\Tests\TestCase;
@@ -128,6 +130,42 @@ class JobStatusCollectionTest extends TestCase
         $this->assertEquals($jobStatus2_1->id, $collection[2]->runs()[0]->jobStatus()->id);
 
         $this->assertEquals($batch1->batch_id, $collection[3]->batchId());
+        $this->assertCount(2, $collection[3]->runs());
+        $this->assertEquals($jobStatus1_2->id, $collection[3]->runs()[0]->jobStatus()->id);
+        $this->assertEquals($jobStatus1_1->id, $collection[3]->runs()[1]->jobStatus()->id);
+    }
+
+    /** @test */
+    public function it_returns_all_jobs_grouped_by_queue()
+    {
+        $jobStatus1_1 = JobStatus::factory()->create(['queue' => 'queue1', 'created_at' => Carbon::now()->subHours(6)]);
+        $jobStatus1_2 = JobStatus::factory()->create(['queue' => 'queue1', 'created_at' => Carbon::now()->subHours(5)]);
+        $jobStatus2_1 = JobStatus::factory()->create(['queue' => 'queue2', 'created_at' => Carbon::now()->subHours(4)]);
+        $jobStatus3_1 = JobStatus::factory()->create(['queue' => 'queue3', 'created_at' => Carbon::now()->subHours(3)]);
+        $jobStatus3_2 = JobStatus::factory()->create(['queue' => 'queue3', 'created_at' => Carbon::now()->subHours(2)]);
+        $jobStatus4 = JobStatus::factory()->create(['queue' => 'queue4', 'created_at' => Carbon::now()->subHours(1)]);
+
+        $collection = JobStatus::all()
+            ->queues();
+
+        $this->assertCount(4, $collection);
+        $this->assertInstanceOf(QueueCollection::class, $collection);
+        $this->assertContainsOnlyInstancesOf(Queue::class, $collection);
+
+        $this->assertEquals('queue4', $collection[0]->name());
+        $this->assertCount(1, $collection[0]->runs());
+        $this->assertEquals($jobStatus4->id, $collection[0]->runs()[0]->jobStatus()->id);
+
+        $this->assertEquals('queue3', $collection[1]->name());
+        $this->assertCount(2, $collection[1]->runs());
+        $this->assertEquals($jobStatus3_2->id, $collection[1]->runs()[0]->jobStatus()->id);
+        $this->assertEquals($jobStatus3_1->id, $collection[1]->runs()[1]->jobStatus()->id);
+
+        $this->assertEquals('queue2', $collection[2]->name());
+        $this->assertCount(1, $collection[2]->runs());
+        $this->assertEquals($jobStatus2_1->id, $collection[2]->runs()[0]->jobStatus()->id);
+
+        $this->assertEquals('queue1', $collection[3]->name());
         $this->assertCount(2, $collection[3]->runs());
         $this->assertEquals($jobStatus1_2->id, $collection[3]->runs()[0]->jobStatus()->id);
         $this->assertEquals($jobStatus1_1->id, $collection[3]->runs()[1]->jobStatus()->id);
---
+++ tests/Unit/Search/Result/QueueTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace JobStatus\Tests\Unit\Search\Result;
+
+use Carbon\Carbon;
+use JobStatus\Enums\Status;
+use JobStatus\Models\JobQueue;
+use JobStatus\Models\JobStatus;
+use JobStatus\Search\Collections\JobRunCollection;
+use JobStatus\Search\Collections\JobStatusCollection;
+use JobStatus\Search\Result\Queue;
+use JobStatus\Search\Result\JobRun;
+use JobStatus\Tests\TestCase;
+
+class QueueTest extends TestCase
+{
+    /** @test */
+    public function runs_gets_the_runs()
+    {
+        $runs = JobStatus::factory()->count(4)->create(['queue' => 'my-queue'])->runs();
+
+        $queue = new Queue('my-queue', $runs);
+
+        $collection = $queue->runs();
+
+        $this->assertCount(4, $collection);
+        $this->assertInstanceOf(JobRunCollection::class, $collection);
+        $this->assertContainsOnlyInstancesOf(JobRun::class, $collection);
+
+        $this->assertEquals($runs->pluck('id')->sort()->values(), $collection->pluck('id')->sort()->values());
+    }
+
+    /** @test */
+    public function it_can_be_cast_with_to_array_and_to_json()
+    {
+        $runs = JobStatus::factory()->count(4)->create(['queue' => 'my-queue', 'status' => Status::QUEUED])
+            ->merge(JobStatus::factory()->count(40)->create(['queue' => 'my-queue', 'status' => Status::STARTED]))
+            ->merge(JobStatus::factory()->count(23)->create(['queue' => 'my-queue', 'status' => Status::FAILED]))
+            ->merge(JobStatus::factory()->count(12)->create(['queue' => 'my-queue', 'status' => Status::SUCCEEDED]))
+            ->merge(JobStatus::factory()->count(2)->create(['queue' => 'my-queue', 'status' => Status::CANCELLED]))
+            ->runs();
+
+        $queue = new Queue('my-queue', $runs);
+
+        $attributes = [
+            'count' => 81,
+            'runs' => $runs->toArray(),
+            'name' => 'my-queue',
+            'queued' => 4,
+            'started' => 40,
+            'failed' => 23,
+            'succeeded' => 12,
+            'cancelled' => 2,
+        ];
+
+        $this->assertEquals($attributes, $queue->toArray());
+        $this->assertEquals(json_encode($attributes), $queue->toJson());
+    }
+
+    /** @test */
+    public function name_gets_the_queue_name()
+    {
+        $runs = JobStatus::factory()->count(4)->create(['queue' => 'my-queue'])->runs();
+
+        $queue = new Queue('my-queue', $runs);
+
+        $queueName = $queue->name();
+
+        $this->assertEquals('my-queue', $queueName);
+    }
+
+    /** @test */
+    public function count_runs_with_status_gets_the_count_of_jobs_with_the_status()
+    {
+        $runs = JobStatus::factory()->count(4)->create(['queue' => 'my-queue', 'status' => Status::QUEUED])
+            ->merge(JobStatus::factory()->count(40)->create(['queue' => 'my-queue', 'status' => Status::STARTED]))
+            ->merge(JobStatus::factory()->count(23)->create(['queue' => 'my-queue', 'status' => Status::FAILED]))
+            ->merge(JobStatus::factory()->count(12)->create(['queue' => 'my-queue', 'status' => Status::SUCCEEDED]))
+            ->merge(JobStatus::factory()->count(2)->create(['queue' => 'my-queue', 'status' => Status::CANCELLED]))
+            ->runs();
+
+        $queue = new Queue('my-queue', $runs);
+
+        $this->assertEquals(4, $queue->countRunsWithStatus(Status::QUEUED));
+        $this->assertEquals(40, $queue->countRunsWithStatus(Status::STARTED));
+        $this->assertEquals(23, $queue->countRunsWithStatus(Status::FAILED));
+        $this->assertEquals(12, $queue->countRunsWithStatus(Status::SUCCEEDED));
+        $this->assertEquals(2, $queue->countRunsWithStatus(Status::CANCELLED));
+    }
+}
