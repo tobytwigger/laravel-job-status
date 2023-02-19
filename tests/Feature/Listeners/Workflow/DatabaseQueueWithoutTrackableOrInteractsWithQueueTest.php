e5769b2d67e19394c035f5c2800dd47fcd206596
tobytwigger <tobytwigger@users.noreply.github.com>
2023-02-19 02:49:32 +0000

    [CI - Linting] Run cs-fixer and eslint

--- tests/Feature/Listeners/Workflow/DatabaseQueueWithoutTrackableOrInteractsWithQueueTest.php
+++ tests/Feature/Listeners/Workflow/DatabaseQueueWithoutTrackableOrInteractsWithQueueTest.php
@@ -2,21 +2,15 @@
 
 namespace JobStatus\Tests\Feature\Listeners\Workflow;
 
-use Illuminate\Support\Facades\Bus;
 use Illuminate\Testing\Assert;
-use JobStatus\Enums\MessageType;
-use JobStatus\Enums\Status;
 use JobStatus\Models\JobBatch;
 use JobStatus\Models\JobStatus;
-use JobStatus\Tests\fakes\AssertBatch;
-use JobStatus\Tests\fakes\AssertJobStatus;
 use JobStatus\Tests\fakes\JobFakeFactory;
 use JobStatus\Tests\fakes\JobFakeWithoutTrackableOrInteractsWithQueue;
 use JobStatus\Tests\TestCase;
 
 class DatabaseQueueWithoutTrackableOrInteractsWithQueueTest extends TestCase
 {
-
     /** @test */
     public function a_run_is_handled()
     {
@@ -303,6 +297,4 @@ class DatabaseQueueWithoutTrackableOrInteractsWithQueueTest extends TestCase
     {
         throw new \Exception('Test');
     }
-
-
 }
