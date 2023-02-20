b65391556fe5728289e552cef01982850b275cb4
Toby Twigger <tobytwigger1@gmail.com>
2023-02-20 00:26:23 +0000

    Add tooltip to status count

--- dashboard/src/components/StatusCount.vue
+++ dashboard/src/components/StatusCount.vue
@@ -5,14 +5,17 @@
       {{ props.label }}
     </q-chip>
     <q-chip v-else dense :color="props.color" text-color="white">
-      <q-avatar :color="color" text-color="white">{{ props.count }}</q-avatar>
-      {{ props.label }}
+      <q-avatar :color="color" text-color="white"></q-avatar>
+      <span>{{ props.label }}</span>
+       <q-tooltip>
+        {{ props.count }}
+       </q-tooltip>
     </q-chip>
   </span>
 </template>
 
 <script setup lang="ts">
-import { defineProps } from 'vue';
+import {defineProps} from 'vue';
 
 const props = defineProps<{
   count: number;
