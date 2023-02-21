08522175994a8848eeb4bc54a2265e0ea47692e2
Toby Twigger <tobytwigger1@gmail.com>
2023-02-21 19:08:10 +0000

    Add more indexes

--- database/migrations/2023_02_21_175500_add_indexes.php
+++ database/migrations/2023_02_21_175500_add_indexes.php
@@ -13,26 +13,26 @@ return new class() extends Migration {
     {
         Schema::table(sprintf('%s_%s', config('laravel-job-status.table_prefix'), 'job_statuses'), function (Blueprint $table) {
             // Helps to calculate the number of jobs of a certain type in a queue
-            $table->index(['queue', 'status', 'selector'], algorithm: 'hash');
+            $table->unique(['queue', 'status', 'selector', 'id'], algorithm: 'hash');
 
             // Helps to calculate the number of jobs of a certain type in a queue
-            $table->index(['batch_id', 'status', 'selector'], algorithm: 'hash');
+            $table->unique(['batch_id', 'status', 'selector', 'id'], algorithm: 'hash');
 
             // Helps to calculate the number of jobs of a certain type in a queue
-            $table->index(['alias', 'status', 'selector'], algorithm: 'hash');
+            $table->unique(['alias', 'status', 'selector', 'id'], algorithm: 'hash');
 
 
 //            // Queues are grouped by queue, and therefore this index speeds the rows scanned from x to y.
-            $table->index(['queue', 'selector', 'created_at', 'id'], algorithm: 'hash');
+            $table->unique(['queue', 'selector', 'created_at', 'id'], algorithm: 'hash');
 //
 //            // Jobs are grouped by alias, and therefore this index speeds the rows scanned from x to y.
-            $table->index(['alias','selector', 'created_at', 'id'], algorithm: 'hash');
+            $table->unique(['alias','selector', 'created_at', 'id'], algorithm: 'hash');
 //
 //            // Batches are grouped by batch_id, and therefore this index speeds the rows scanned from x to y.
-            $table->index(['batch_id','selector', 'created_at', 'id'], algorithm: 'hash');
+            $table->unique(['batch_id','selector', 'created_at', 'id'], algorithm: 'hash');
 //
 //             // We nearly always order by created_at and id, so this index speeds that up.
-            $table->index(['selector', 'created_at', 'id'], algorithm: 'hash');
+            $table->unique(['selector', 'created_at', 'id'], algorithm: 'hash');
         });
     }
 
@@ -43,24 +43,24 @@ return new class() extends Migration {
     public function down()
     {
         Schema::table(sprintf('%s_%s', config('laravel-job-status.table_prefix'), 'job_statuses'), function (Blueprint $table) {
-            $table->dropIndex(['queue', 'status', 'selector']);
+            $table->dropUnique(['queue', 'status', 'selector', 'id']);
 
-            $table->dropIndex(['batch_id', 'status', 'selector']);
+            $table->dropUnique(['batch_id', 'status', 'selector', 'id']);
 
-            $table->dropIndex(['alias', 'status', 'selector']);
+            $table->dropUnique(['alias', 'status', 'selector', 'id']);
 
 
 //            // Queues are grouped by queue, and therefore this index speeds the rows scanned from x to y.
-            $table->dropIndex(['queue', 'selector', 'created_at', 'id']);
+            $table->dropUnique(['queue', 'selector', 'created_at', 'id']);
 //
 //            // Jobs are grouped by alias, and therefore this index speeds the rows scanned from x to y.
-            $table->dropIndex(['alias', 'selector', 'created_at', 'id']);
+            $table->dropUnique(['alias', 'selector', 'created_at', 'id']);
 //
 //            // Batches are grouped by batch_id, and therefore this index speeds the rows scanned from x to y.
-            $table->dropIndex(['batch_id', 'selector', 'created_at', 'id']);
+            $table->dropUnique(['batch_id', 'selector', 'created_at', 'id']);
 //
 //            // We nearly always order by created_at and id, so this index speeds that up.
-            $table->dropIndex(['selector', 'status']);
+            $table->dropUnique(['selector', 'created_at', 'id']);
         });
     }
 };
